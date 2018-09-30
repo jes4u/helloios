34a184128970be9e92c79bd70944afb8d1224b5a
iosdev <iosdev@Felixs-13-MBP.local>
2018-09-30 02:15:02 -0700

    First push

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by iGuest on 9/29/18.
+//  Copyright © 2018 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
